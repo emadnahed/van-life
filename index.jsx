6f757afe055bdcf9ce3cb91334520d2c9b635a9c
emaadnahed <emaadnahed@gmail.com>
2024-02-13 07:39:36 +0530

    re-fix of the notfound page

--- index.jsx
+++ index.jsx
@@ -49,7 +49,7 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
-          <Route path="*" element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
